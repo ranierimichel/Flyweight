9042869926f3e768859101a0e92efaae880f4bdc
Ranieri Michel Alves Silva <ranieri.michel@gmail.com>
2021-07-13 12:34:48 -0300

    Publish Flyweight Pattern Examples

---
+++ FlyweightExercise/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FlyweightExercise
+{
+    public class Sentence
+    {
+        private string PlainText;
+        private string[] ReturnText;
+        private WordToken[] WT;
+
+        public Sentence(string plainText)
+        {
+            this.PlainText = plainText;
+            ReturnText = PlainText.Split(' ');
+            WT = new WordToken[ReturnText.Length];
+            for (int i = 0; i < WT.Length; i++)
+            {
+                WT[i] = new WordToken();
+            }
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                WordToken _wordToken = new WordToken();
+                WT[index] = _wordToken;
+                return _wordToken;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < ReturnText.Length; i++)
+            {
+                sb.Append(WT[i].Capitalize.Equals(true)
+                    ? $"{ReturnText[i].ToUpper()}"
+                    : $"{ReturnText[i].ToLower()}");
+                if (i < ReturnText.Length - 1)
+                    sb.Append(' ');
+            }
+            return sb.ToString();
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = new Sentence("hello world");
+            sentence[1].Capitalize = true;
+            WriteLine(sentence);
+        }
+    }
+}
